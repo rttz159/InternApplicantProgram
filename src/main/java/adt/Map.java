50254522ca337be191867f0be1ae0c54674cf06d
Raymond Teng <raintaroteng@gmail.com>
2025-02-27 01:23:06 +0800

    Map Interface

---
+++ src/main/java/adt/Map.java
@@ -0,0 +1,25 @@
+package adt;
+
+/**
+ *
+ * @author rttz159
+ */
+public interface Map<K, V> {
+    
+    void put(K key, V value);
+    V get(K key);
+    V remove(K key);
+    boolean containsKey(K key);
+    boolean containsValue(V value);
+    int size();
+    boolean isEmpty();
+    Set<K> keySet();
+    List<V> values();
+    Set<Entry<K, V>> entrySet();
+
+    interface Entry<K, V> {
+        K getKey();
+        V getValue();
+        V setValue(V value);
+    }
+}
